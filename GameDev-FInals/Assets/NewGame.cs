b3c517d53f036e6d0392a5ee249a34c5dd6cbb69
Mary Rose Nogoy <Maryrosenogoy3@gmail.com>
2022-12-05 22:02:18 +0800

    upload player prefs

---
+++ GameDev-FInals/Assets/NewGame.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour
+{
+    
+  public void playGame()
+  {
+    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+  }
+
+  public void Back()
+  {
+    // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+      SceneManager.LoadScene("level4");
+  }
+}
